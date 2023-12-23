11e2020da41795f0c8d2b409caada0a1c3c139e1
Rajarshi Das <rd@rajarshi.me>
2023-12-23 16:30:35 +0530

    back button

--- w3root/friends/friend-add.php
+++ w3root/friends/friend-add.php
@@ -2,17 +2,21 @@
 
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
+$btn = $_POST['btn'];
 
-// 1. Connect to database
-$db = new SQLite3('friends.db');
+if ($btn === 'Add') {
 
-// 2. Query statement
-$sql = "INSERT INTO friends (`fname`, `lname`) values ('$fname', '$lname')";
-echo $sql;
+    // 1. Connect to database
+    $db = new SQLite3('friends.db');
 
-// 3. Save in database
-$db->exec('BEGIN');
-$db->query($sql);
-$db->exec('COMMIT');
+    // 2. Query statement
+    $sql = "INSERT INTO friends (`fname`, `lname`) values ('$fname', '$lname')";
+    echo $sql;
+
+    // 3. Save in database
+    $db->exec('BEGIN');
+    $db->query($sql);
+    $db->exec('COMMIT');
+}
 
 header("Location:index.php");
